eb8b764633a0962004536ba58e879a4848b50ee7
mohosinmiah <hamza1610330816@gmail.com>
2023-06-17 14:16:02 +0600

    Initial project setup added

---
+++ topic01-mohosinmiah/topic01/src/test/java/com/topic01/topic01/Topic01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topic01.topic01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
