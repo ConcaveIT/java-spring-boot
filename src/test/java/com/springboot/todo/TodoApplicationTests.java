daf0f221bdf3a380fb2e831790593c28892a12d1
Rubyet <rubayet@workspaceit.com>
2023-06-17 22:28:36 +0600

    initial project setup

---
+++ src/test/java/com/springboot/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
