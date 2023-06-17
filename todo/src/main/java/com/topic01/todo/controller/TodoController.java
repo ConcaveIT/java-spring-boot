06dad68f73bb8a7f2eb29237d40aef6e9b5b327c
Tohir Raihan <tohir.raihan@gmail.com>
2023-06-17 17:56:24 +0600

    Update TodoController and added saveTodo method with "/todos" end point with post method

---
+++ todo/src/main/java/com/topic01/todo/controller/TodoController.java
@@ -0,0 +1,27 @@
+package com.topic01.todo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.topic01.todo.entity.Todo;
+import com.topic01.todo.service.TodoService;
+
+@RestController
+public class TodoController {
+
+  @Autowired
+  private TodoService todoService;
+
+  @PostMapping("/todos")
+  public Todo saveTodo(@RequestBody Todo todo) {
+    return todoService.saveTodo(todo);
+  }
+
+  @GetMapping("/test")
+  public String test() {
+    return "testing";
+  }
+}
