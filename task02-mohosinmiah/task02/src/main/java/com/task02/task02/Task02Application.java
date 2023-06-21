bab2c6f16df1c26a12c4a09f287e70df2251883a
mohosinmiah <hamza1610330816@gmail.com>
2023-06-22 00:16:46 +0600

    Project Initial Setup Done

---
+++ task02-mohosinmiah/task02/src/main/java/com/task02/task02/Task02Application.java
@@ -0,0 +1,13 @@
+package com.task02.task02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task02Application.class, args);
+	}
+
+}
---
+++ task02-mohosinmiah/task02/src/test/java/com/task02/task02/Task02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task02.task02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
