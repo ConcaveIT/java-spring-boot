128d35a3ebc7c4e8c01f5c7c619aae297572bcd7
Tohir Raihan <tohir.raihan@gmail.com>
2023-06-24 12:15:07 +0600

    Impliment create Payment Type successfully

--- payment-gateway/src/main/java/com/topic02/paymentgateway/controller/PaymentTypeController.java
+++ payment-gateway/src/main/java/com/topic02/paymentgateway/controller/PaymentTypeController.java
@@ -1,16 +1,24 @@
 package com.topic02.paymentgateway.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.topic02.paymentgateway.entity.PaymentType;
+import com.topic02.paymentgateway.service.PaymentTypeService;
+
 @RestController
-@RequestMapping("/payment-types")
+// @RequestMapping("/payment-types")
 public class PaymentTypeController {
 
-  @GetMapping("")
-  public String test() {
-    return "Testing";
+  @Autowired
+  private PaymentTypeService paymentTypeService;
+
+  @PostMapping("/payment-types")
+  public PaymentType savePaymentType(@RequestBody PaymentType paymentType) {
+    return paymentTypeService.savePaymentType(paymentType);
+    // return paymentType;
   }
 
 }
--- payment-gateway/src/main/java/com/topic02/paymentgateway/service/PaymentTypeServiceImpl.java
+++ payment-gateway/src/main/java/com/topic02/paymentgateway/service/PaymentTypeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.topic02.paymentgateway.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.topic02.paymentgateway.entity.PaymentType;
@@ -8,7 +9,8 @@ import com.topic02.paymentgateway.repository.PaymentTypeRepository;
 @Service
 public class PaymentTypeServiceImpl implements PaymentTypeService {
 
-  PaymentTypeRepository paymentTypeRepository;
+  @Autowired
+  private PaymentTypeRepository paymentTypeRepository;
 
   @Override
   public PaymentType savePaymentType(PaymentType paymentType) {
