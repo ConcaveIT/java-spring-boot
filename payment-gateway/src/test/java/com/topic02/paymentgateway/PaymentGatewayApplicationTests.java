0a8b4a0a940f9626c1df1c884cd6e61564f66598
Tohir Raihan <tohir.raihan@gmail.com>
2023-06-24 10:48:29 +0600

    Initial commit with necessary dependencies and project setup

---
+++ payment-gateway/src/test/java/com/topic02/paymentgateway/PaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topic02.paymentgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
