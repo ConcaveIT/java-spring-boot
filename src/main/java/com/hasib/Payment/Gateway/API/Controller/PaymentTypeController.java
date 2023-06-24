18a7436a5f6e2b7459bed9f06ed6130b8d0f0072
Hasibul Hasan <hasibulhasan442@gmail.com>
2023-06-24 10:12:46 +0600

    CURD opration Done

--- src/main/java/com/hasib/Payment/Gateway/API/Controller/PaymentTypeController.java
+++ src/main/java/com/hasib/Payment/Gateway/API/Controller/PaymentTypeController.java
@@ -1,6 +1,7 @@
 package com.hasib.Payment.Gateway.API.Controller;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -8,8 +9,11 @@ import javax.validation.Valid;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hasib.Payment.Gateway.API.Dto.PaymentTypeRequestDto;
 import com.hasib.Payment.Gateway.API.Service.PaymentTypeService;
 
-
-
 @RestController
 @RequestMapping("/api/v1/payment-type")
 public class PaymentTypeController {
-    
+
     private PaymentTypeService paymentTypeService;
 
     public PaymentTypeController(PaymentTypeService paymentTypeService) {
@@ -35,21 +37,64 @@ public class PaymentTypeController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Object> createPaymentType(@Valid @RequestBody PaymentTypeRequestDto paymentTypeRequestDto, BindingResult bindingResult) {
-        // try{
-            if(bindingResult.hasErrors()) {
+    public ResponseEntity<?> createPaymentType(@Valid @RequestBody PaymentTypeRequestDto paymentTypeRequestDto,
+            BindingResult bindingResult) {
+        try {
+            if (bindingResult.hasErrors()) {
 
                 String errorMessage = bindingResult.getAllErrors().stream()
                         .map(DefaultMessageSourceResolvable::getDefaultMessage)
                         .collect(Collectors.joining(", "));
-            return ResponseEntity.badRequest().body(Collections.singletonMap("error", errorMessage));
-            }
-            else {
+                return ResponseEntity.badRequest().body(Collections.singletonMap("error", errorMessage));
+            } else {
                 PaymentTypeRequestDto createPaymentType = paymentTypeService.createPaymentType(paymentTypeRequestDto);
                 return ResponseEntity.ok(createPaymentType);
             }
-        // } catch (Exception e) {
-        //     return ResponseEntity.badRequest().body(e.getMessage());
-        // }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PaymentTypeRequestDto>> getAllPaymentType() {
+        try {
+            List<PaymentTypeRequestDto> paymentTypeRequestDto = paymentTypeService.getAllPaymentType();
+            return ResponseEntity.ok(paymentTypeRequestDto);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Collections.singletonList(new PaymentTypeRequestDto()));
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PaymentTypeRequestDto> getPaymentTypeById(@PathVariable Long id) {
+
+        PaymentTypeRequestDto paymentTypeRequestDto = paymentTypeService.getPaymentTypeById(id);
+        if (paymentTypeRequestDto != null) {
+            return ResponseEntity.ok(paymentTypeRequestDto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<PaymentTypeRequestDto> updatePaymentType(@PathVariable Long id,
+            @Valid @RequestBody PaymentTypeRequestDto paymentTypeRequestDto, BindingResult bindingResult) {
+
+        PaymentTypeRequestDto updatePaymentType = paymentTypeService.updatePaymentType(id, paymentTypeRequestDto);
+        if (updatePaymentType != null) {
+            return ResponseEntity.ok(updatePaymentType);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deletePaymentType(@PathVariable Long id) {
+        boolean isDeleted = paymentTypeService.deletePaymentType(id);
+        if (isDeleted) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/hasib/Payment/Gateway/API/Service/PaymentTypeService.java
+++ src/main/java/com/hasib/Payment/Gateway/API/Service/PaymentTypeService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import com.hasib.Payment.Gateway.API.Dto.PaymentTypeRequestDto;
 import com.hasib.Payment.Gateway.API.Entity.PaymentType;
 import com.hasib.Payment.Gateway.API.Repository.PaymentRepository;
-
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 @Service
 public class PaymentTypeService {
     private PaymentRepository paymentRepository;
@@ -34,5 +36,68 @@ public class PaymentTypeService {
         paymentTypeRequestDtos.setDescription(paymentTypes.getDescription());
 
         return paymentTypeRequestDtos;
+    }
+
+    public List<PaymentTypeRequestDto> getAllPaymentType() {
+        List<PaymentType> paymentTypes = paymentRepository.findAll();
+        List<PaymentTypeRequestDto> paymentTypeRequestDtos = paymentTypes.stream().map(paymentType -> {
+            PaymentTypeRequestDto paymentTypeRequestDto = new PaymentTypeRequestDto();
+            paymentTypeRequestDto.setId(paymentType.getId());
+            paymentTypeRequestDto.setName(paymentType.getName());
+            paymentTypeRequestDto.setDescription(paymentType.getDescription());
+            return paymentTypeRequestDto;
+        }).collect(Collectors.toList());
+        return paymentTypeRequestDtos;
+    }
+
+    public PaymentTypeRequestDto getPaymentTypeById(Long id) {
+        // PaymentType paymentType = paymentRepository.findById(id).orElseThrow(() -> new RuntimeException("Payment Type not found"));
+        // PaymentTypeRequestDto paymentTypeRequestDto = new PaymentTypeRequestDto();
+        // paymentTypeRequestDto.setId(paymentType.getId());
+        // paymentTypeRequestDto.setName(paymentType.getName());
+        // paymentTypeRequestDto.setDescription(paymentType.getDescription());
+        // return paymentTypeRequestDto;
+        Optional<PaymentType> paymentType = paymentRepository.findById(id);
+        if(paymentType.isPresent()) {
+            PaymentType paymentDetails = paymentType.get();
+            PaymentTypeRequestDto paymentTypeRequestDto = new PaymentTypeRequestDto();
+            paymentTypeRequestDto.setId(paymentDetails.getId());
+            paymentTypeRequestDto.setName(paymentDetails.getName());
+            paymentTypeRequestDto.setDescription(paymentDetails.getDescription());
+            return paymentTypeRequestDto;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public PaymentTypeRequestDto updatePaymentType(Long id, PaymentTypeRequestDto paymentTypeRequestDto) {
+        Optional<PaymentType> paymentType = paymentRepository.findById(id);
+        if(paymentType.isPresent()) {
+            PaymentType paymentDetails = paymentType.get();
+            paymentDetails.setName(paymentTypeRequestDto.getName());
+            paymentDetails.setDescription(paymentTypeRequestDto.getDescription());
+            PaymentType updatedPaymentType = paymentRepository.save(paymentDetails);
+
+            PaymentTypeRequestDto paymentTypeRequestDtos = new PaymentTypeRequestDto();
+            paymentTypeRequestDtos.setId(updatedPaymentType.getId());
+            paymentTypeRequestDtos.setName(updatedPaymentType.getName());
+            paymentTypeRequestDtos.setDescription(updatedPaymentType.getDescription());
+            return paymentTypeRequestDtos;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public boolean deletePaymentType(Long id) {
+        Optional<PaymentType> paymentType = paymentRepository.findById(id);
+        if(paymentType.isPresent()) {
+            paymentRepository.delete(paymentType.get());
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
