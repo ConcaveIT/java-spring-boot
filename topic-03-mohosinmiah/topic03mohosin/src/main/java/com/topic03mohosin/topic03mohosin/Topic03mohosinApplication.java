a71ea3f95fabe0a3590d724d0d47d356fea66c72
mohosinmiah <hamza1610330816@gmail.com>
2023-06-27 18:44:45 +0600

    topic-03 project structure added

---
+++ topic-03-mohosinmiah/topic03mohosin/src/main/java/com/topic03mohosin/topic03mohosin/Topic03mohosinApplication.java
@@ -0,0 +1,13 @@
+package com.topic03mohosin.topic03mohosin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic03mohosinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Topic03mohosinApplication.class, args);
+	}
+
+}
---
+++ topic-03-mohosinmiah/topic03mohosin/src/test/java/com/topic03mohosin/topic03mohosin/Topic03mohosinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topic03mohosin.topic03mohosin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic03mohosinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
