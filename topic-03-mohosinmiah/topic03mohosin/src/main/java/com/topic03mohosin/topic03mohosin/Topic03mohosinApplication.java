a16bced11efad65e3f34e491cc043cb10c30d371
mohosinmiah <hamza1610330816@gmail.com>
2023-06-27 18:49:08 +0600

    Added necessary dependency and configuration for model mapper

--- topic-03-mohosinmiah/topic03mohosin/src/main/java/com/topic03mohosin/topic03mohosin/Topic03mohosinApplication.java
+++ topic-03-mohosinmiah/topic03mohosin/src/main/java/com/topic03mohosin/topic03mohosin/Topic03mohosinApplication.java
@@ -1,10 +1,18 @@
 package com.topic03mohosin.topic03mohosin;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Topic03mohosinApplication {
+
+	@Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Topic03mohosinApplication.class, args);
