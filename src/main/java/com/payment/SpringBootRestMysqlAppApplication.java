ac3601aed2afad7605391b5ccdbff5a81e520255
Sfsohel <sfsohel1895@gmail.com>
2023-08-05 00:07:50 +0600

    first file

---
+++ src/main/java/com/payment/SpringBootRestMysqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestMysqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestMysqlAppApplication.class, args);
+	}
+
+}
